41df8e59575f7d1fc5c65e3212ad778d82d451ae
StormedBubbles <80055191+StormedBubbles@users.noreply.github.com>
2022-09-29 21:00:28 -0400

    Update MMInputSystem.h

--- Src/OSD/MM/MMInputSystem.h
+++ Src/OSD/MM/MMInputSystem.h
@@ -37,8 +37,8 @@
 
 #define WIN32_LEAN_AND_MEAN
 //#include <windows.h>
-#include <dinput.h>
-#include <XInput.h>
+//#include <dinput.h>
+//#include <XInput.h>
 #include <functional>
 
 #include <vector>
